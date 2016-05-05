51e20776922071b562b8a16922060925ff00b209
reed-jpm-alpert <reed.alpert@jpmorgan.com>
2016-05-05 12:33:30 -0400

    commit files

---
+++ mamda/c_cpp/src/cpp/version.h
@@ -0,0 +1,9 @@
+/* This file was automatically generated */
+#ifndef MamdaVersionHH
+#define MamdaVersionHH
+#define MAMDA_VERSION_MAJOR 2
+#define MAMDA_VERSION_MINOR 3
+#define MAMDA_VERSION_RELEASE 3
+#define MAMDA_VERSION "mamda 2.3.3"
+extern const char* mamda_version;
+#endif
